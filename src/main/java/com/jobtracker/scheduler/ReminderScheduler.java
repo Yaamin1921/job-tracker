ff9c8fe09feacfe2e1d1716eb4fbe38d8ed87866
Yaamin1921 <100552714+Yaamin1921@users.noreply.github.com>
2026-04-25 17:06:42 +0530

    Tech/chore (#18)
    * fix: rename yaml to yml
    
    * chore: disable schedular to set up deploy

--- src/main/java/com/jobtracker/scheduler/ReminderScheduler.java
+++ src/main/java/com/jobtracker/scheduler/ReminderScheduler.java
@@ -12,7 +12,7 @@ public class ReminderScheduler {
     private final ReminderService reminderService;
 
     //@Scheduled(fixedRate = 60000)
-    @Scheduled(fixedDelay = 60000)
+    //@Scheduled(fixedDelay = 60000)
     public void runReminderJob() {
         reminderService.processReminders();
     }
