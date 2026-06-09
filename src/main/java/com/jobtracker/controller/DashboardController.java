03d7cebd2a29534ea03150f4c7f43332b84c390c
Mohd Yaamin <yaamin1921413@gmail.com>
2026-06-09 10:26:48 +0530

    feat(dashboard-analytics): add service layer logic to get dashboard analytic.

--- src/main/java/com/jobtracker/controller/DashboardController.java
+++ src/main/java/com/jobtracker/controller/DashboardController.java
@@ -1,7 +1,12 @@
 package com.jobtracker.controller;
 
+import com.jobtracker.dto.DashboardResponse;
+import com.jobtracker.service.DashboardService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/dashboard")
 @Tag(name="Dashboard")
 public class DashboardController {
+    private final DashboardService dashboardService;
 
+    public DashboardController(DashboardService dashboardService) {
+        this.dashboardService = dashboardService;
+    }
+
+    @GetMapping
+    public ResponseEntity<DashboardResponse> getStats() {
+        return ResponseEntity.ok(
+                dashboardService.getDashboardStats()
+        );
+    }
 }
--- src/main/java/com/jobtracker/service/impl/DashboardServiceImpl.java
+++ src/main/java/com/jobtracker/service/impl/DashboardServiceImpl.java
@@ -1,6 +1,54 @@
 package com.jobtracker.service.impl;
 
+import com.jobtracker.dto.DashboardResponse;
+import com.jobtracker.entity.JobStatus;
+import com.jobtracker.repository.JobRepository;
 import com.jobtracker.service.DashboardService;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class DashboardServiceImpl implements DashboardService {
+    private final JobRepository repository;
+
+    public DashboardResponse getDashboardStats() {
+
+        long totalJobs = repository.count();
+
+        long appliedCount =
+                repository.countByStatus(JobStatus.APPLIED);
+
+        long rejectedCount =
+                repository.countByStatus(JobStatus.REJECTED);
+
+        long responseCount =
+                repository.countByStatusIn(
+                        List.of(
+                                JobStatus.INTERVIEW,
+                                JobStatus.OFFERED
+                        ));
+
+        double responseRate = totalJobs == 0
+                ? 0
+                : ((double) responseCount / totalJobs) * 100;
+
+        double rejectedPercentage = totalJobs == 0
+                ? 0
+                : ((double) rejectedCount / totalJobs) * 100;
+
+        return DashboardResponse.builder()
+                .totalJobs(totalJobs)
+                .appliedCount(appliedCount)
+                .rejectedCount(rejectedCount)
+                .responseRate(
+                        Math.round(responseRate * 100.0) / 100.0)
+                .rejectedPercentage(
+                        Math.round(rejectedPercentage * 100.0) / 100.0)
+                .build();
+    }
 }
+
